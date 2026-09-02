a6c92e53f2c423e7fb7f203eac8f04dbb3d73577
Federico Lochbaum <federico.lochbaum@gmail.com>
2026-09-02 16:49:01 +0200

    adding new scripts

---
+++ perf-muts-scripts/run-all-schedulers-regexes-with-different-seeds.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# run-all-parallel.sh
+# Launches Pharo experiments in parallel, each downloading its own environment.
+
+scripts=(
+    "run-regexes-weighted-scheduling.st"
+    "run-regexes-random-scheduling.st"
+    "run-regexes-mopt-scheduling.st"
+
+    "run-regexes-weighted-scheduling-2-seeds.st"
+    "run-regexes-random-scheduling-2-seeds.st"
+    "run-regexes-mopt-scheduling-2-seeds.st"
+)
+
+folders=(
+    "regexes-weighted-scheduling-100-seeds"
+    "regexes-random-scheduling-100-seeds"
+    "regexes-mopt-scheduling-100-seeds"
+
+    "regexes-weighted-scheduling-2-seeds"
+    "regexes-random-scheduling-2-seeds"
+    "regexes-mopt-scheduling-2-seeds"
+)
+
+BASE_DIR="$(pwd)"
+
+for i in "${!scripts[@]}"; do
+    script="${scripts[$i]}"
+    run_dir="${folders[$i]}"
+    
+    echo "Queueing $script in isolated directory $run_dir"
+    
+    mkdir -p "$run_dir"
+    (
+        cd "$run_dir"
+        ../run.sh "../$script" > "../$script.log" 2>&1
+    ) &
+done
+
+echo "All experiments launched in parallel. Check *.log files for updates."
+echo "Waiting for all processes to finish..."
+wait
+echo "All experiments finished."
